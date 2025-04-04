e0568c5236f046a95760bf8a95f3f920b800d95c
charlesaraya <charlesaraya@gmail.com>
2025-04-04 10:45:06 +0200

    feat: add welcome and initialization workflow into graph

--- src/graph.py
+++ src/graph.py
@@ -1,31 +1,73 @@
+import json
+
 from langgraph.graph import MessagesState, StateGraph, START, END
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import SystemMessage
+from langchain_core.messages import SystemMessage, HumanMessage
 
 from prompts import (
-    system_prompt_init
+    PROMPT_SYSTEM_WELCOME,
+    PROMPT_SYSTEM_INIT,
 )
 
-model = ChatOpenAI(model="gpt-4o")
+model = ChatOpenAI(model="gpt-4o", temperature=0)
 
-class InitState(MessagesState):
+def parse_response(content):
+    try:
+        result = json.loads(content)
+    except json.JSONDecodeError as e:
+        print(f"JSON parsing failed. Raw content: {content}")
+        raise ValueError(f"Invalid JSON: {e}")
+    return result
+
+class UserInfoState(MessagesState):
+    name: str
     language: str
     skill_level: str
 
-def initialization_node(state: InitState):
+def welcome_node(state: UserInfoState):
+    messages = [SystemMessage(content=PROMPT_SYSTEM_WELCOME)] + state["messages"]
+    response = model.invoke(messages)
+    return {"messages": response}
+
+def initialize_node(state: UserInfoState):
+    name = state.get("name", "")
     language = state.get("language", "")
     skill_level = state.get("skill_level", "")
 
-    messages = [SystemMessage(content=system_prompt_init)] + state["messages"]
+    messages = [SystemMessage(content=PROMPT_SYSTEM_INIT)]
+    messages.append(state['messages'][-1])
+    #messages.append(HumanMessage(content=f"Last message: {state['messages'][-1]}.\nKnown information: [Name: {"Unknown" if not name else name}, Language: {"Unknown" if not language else language}, Skill Level: {"Unknown" if not skill_level else skill_level}]"))
 
     response = model.invoke(messages)
-    return {"messages": response, "language": language, "skill_level": skill_level}
+    result = parse_response(response.content)
 
-# Create Graph
-builder = StateGraph(InitState)
-builder.add_node("initialization", initialization_node)
-# Logic
-builder.add_edge(START, "initialization")
-builder.add_edge("initialization", END)
-# Compile
-graph = builder.compile()
+    name = result.get("name", "") if not name else name
+    language = result.get("language", "") if not language else language
+    skill_level = result.get("skill_level", "") if not skill_level else skill_level
+
+    return {"messages": state['messages'], "name": name, "language": language, "skill_level": skill_level}
+
+def planning_node(state: UserInfoState):
+    pass
+
+def check_initialization(state: UserInfoState):
+    state["name"] = state.get("name", "")
+    state["language"] = state.get("language", "")
+    state["skill_level"] = state.get("skill_level", "")
+    if state["name"] and state["language"] and state["skill_level"]:
+        return "init_complete"
+    return "init_pending"
+
+def build_graph():
+    # Create Graph
+    builder = StateGraph(UserInfoState)
+    builder.add_node("welcome", welcome_node)
+    builder.add_node("initializer", initialize_node)
+    builder.add_node("planner", planning_node)
+    # Logic
+    builder.add_edge(START, "welcome")
+    builder.add_edge("welcome", "initializer")
+    builder.add_conditional_edges("initializer", check_initialization, {"init_pending": END, "init_complete": "planner"})
+    #builder.add_edge("planner", END)
+    # Compile
+    return builder.compile()
--- src/prompts.py
+++ src/prompts.py
@@ -1,10 +1,23 @@
-system_prompt_init = """
-You are a personal coding mentor. To customize the learning experience of the user your goal is to determine:
+PROMPT_SYSTEM_WELCOME = """
+You are a friendly personal coding mentor. You are CodeGrind, coding agent designed 
+to strengthen your programming fundamentals through deliberate practice.
 
-1. Which programming language would the user like to master? (e.g., Python, JavaScript, C++, ...)
-2. How would they describe their current skill level? Which can be any of:
+Introduce yourself briefly.
+
+To customize the learning experience of the user your goal is to determine:
+1. The name of the user.
+2. Which programming language would the user like to master? (e.g., Python, JavaScript, C++, ...)
+3. How would they describe their current skill level? Which can be any of:
 
 - Beginner: New to coding and learning the basics.
 - Intermediate: They know programming but want to improve their skills and master foundations.
 - Advanced: Are Senior programmers that want to master complex topics.
+
+Don't ask all the questions at the same time. Ask the questions sequentially, after you know the answer.
+"""
+
+PROMPT_SYSTEM_INIT = """
+Extract the user name, programming language, and skill level from the input.
+If any information is not specified, set it to '' (empty string).
+Returns a plain JSON string: {"name": str, "language": str, "skill_level": str}
 """
